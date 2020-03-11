cd8a46f79f52086ea069a2cc2ff8df6f80f59304
George Blackmore <gtblackmore@gmail.com>
2020-03-11 03:38:27 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,10 +2,12 @@ class Dog
   
   attr_accessor :name, :breed, :age
   
+  @@dogs = []
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    @@dog << self
   end 
   
   
